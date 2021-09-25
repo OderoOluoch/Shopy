a3bf639592c16f8ab8e6d17c8ec5a88df35479b8
OderoOluoch <oderoluoch@gmail.com>
2021-09-25 12:51:06 +0300

    Simple interactivity interface in place

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,24 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to Shopy, ");
-        System.out.println("Welcome to Shopy, ");
+        System.out.println("------------------------------------------");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("AVAILABLE ITEMS IN THE CATALOG: ");
+        System.out.println("1. Item: Kabras Sugar Qty: 10");
+        System.out.println("2. Item: Ajab Flour Qty: 10");
+        System.out.println("3. Item: Kimbo Oil Qty: 8");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("--------------------------------------------");
+        System.out.println("PLEASE SELECT ANY OF THE FOLLOWING TO ENJOY OUR SERVICES ");
+        System.out.println("1. Place an Order");
+        System.out.println("2. Cancel the Order");
+        System.out.println("3. View Cart");
+        System.out.println("4. View Orders");
+        System.out.println("5. Exit");
+        System.out.println("6. Please choose option: 1");
+
     }
+
 }
